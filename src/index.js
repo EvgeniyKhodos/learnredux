2471d78654ad2c49782e6e25bc11f1ffff668341
EvgeniyKhodos <en-hodos@mail.ru>
2021-03-14 11:19:13 +0700

    Actions and reducer

--- src/index.js
+++ src/index.js
@@ -1,37 +1,21 @@
 import {createStore} from 'redux';
-
-const reducer = (state = 0, action) => {
-    switch (action.type) {
-        case 'INC':
-            return state + 1;
-        case 'DEC':
-            return state - 1;
-        case 'RND':
-            return state + action.value;
-        default:
-            return state;
-    }
-}
-
-
-
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const rnd = (value) => ({type: 'RND', value});
+import {inc, dec, rnd} from './actions';
+import reducer from "./reducer";
 
 const store = createStore(reducer);
+const {dispatch} = store;
 
 document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc())
+    dispatch(inc())
 });
 
 document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec())
+    dispatch(dec())
 });
 
 document.getElementById('rnd').addEventListener('click', () => {
     const value = Math.floor(Math.random() * 10);
-    store.dispatch(rnd(value));
+    dispatch(rnd(value));
 });
 
 const update = () => {
